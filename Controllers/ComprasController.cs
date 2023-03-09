dba8117f4d43c179ecf0ddc9050468e8651a634c
rpgcunha <rpgcunha@hotmail.com>
2023-03-09 15:56:36 +0000

    criado controladores para colonizar bd

---
+++ Controllers/ComprasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShopShop.Data;
+using ShopShop.Models;
+
+namespace ShopShop.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ComprasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Compras
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Compras.Include(c => c.Cliente);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Compras/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // GET: Compras/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Compras/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ClienteId,Dia,Hora,Total")] Compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(compra);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", compra.ClienteId);
+            return View(compra);
+        }
+
+        // GET: Compras/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras.FindAsync(id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", compra.ClienteId);
+            return View(compra);
+        }
+
+        // POST: Compras/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ClienteId,Dia,Hora,Total")] Compra compra)
+        {
+            if (id != compra.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(compra);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompraExists(compra.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", compra.ClienteId);
+            return View(compra);
+        }
+
+        // GET: Compras/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // POST: Compras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Compras == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Compras'  is null.");
+            }
+            var compra = await _context.Compras.FindAsync(id);
+            if (compra != null)
+            {
+                _context.Compras.Remove(compra);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompraExists(int id)
+        {
+          return _context.Compras.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/ListasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShopShop.Data;
+using ShopShop.Models;
+
+namespace ShopShop.Controllers
+{
+    public class ListasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Listas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Listas.Include(l => l.Produto);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Listas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Listas == null)
+            {
+                return NotFound();
+            }
+
+            var lista = await _context.Listas
+                .Include(l => l.Produto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lista == null)
+            {
+                return NotFound();
+            }
+
+            return View(lista);
+        }
+
+        // GET: Listas/Create
+        public IActionResult Create()
+        {
+            ViewData["ProdutoId"] = new SelectList(_context.Produtos, "Id", "Id");
+            return View();
+        }
+
+        // POST: Listas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProdutoId,Quantidade,Preco,Local")] Lista lista)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lista);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProdutoId"] = new SelectList(_context.Produtos, "Id", "Id", lista.ProdutoId);
+            return View(lista);
+        }
+
+        // GET: Listas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Listas == null)
+            {
+                return NotFound();
+            }
+
+            var lista = await _context.Listas.FindAsync(id);
+            if (lista == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProdutoId"] = new SelectList(_context.Produtos, "Id", "Id", lista.ProdutoId);
+            return View(lista);
+        }
+
+        // POST: Listas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProdutoId,Quantidade,Preco,Local")] Lista lista)
+        {
+            if (id != lista.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lista);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ListaExists(lista.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProdutoId"] = new SelectList(_context.Produtos, "Id", "Id", lista.ProdutoId);
+            return View(lista);
+        }
+
+        // GET: Listas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Listas == null)
+            {
+                return NotFound();
+            }
+
+            var lista = await _context.Listas
+                .Include(l => l.Produto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lista == null)
+            {
+                return NotFound();
+            }
+
+            return View(lista);
+        }
+
+        // POST: Listas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Listas == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Listas'  is null.");
+            }
+            var lista = await _context.Listas.FindAsync(id);
+            if (lista != null)
+            {
+                _context.Listas.Remove(lista);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ListaExists(int id)
+        {
+          return _context.Listas.Any(e => e.Id == id);
+        }
+    }
+}
