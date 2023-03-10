e0d3610669b6ccd65fd1105bafea313042c00fa9
rpgcunha <rpgcunha@hotmail.com>
2023-03-10 09:40:51 +0000

    correcao string connection

---
+++ Migrations/20230310091511_iefp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class iefp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
