5be52ef0a9f10d70d503a7f393bf23430f7a0cf4
rpgcunha <rpgcunha@hotmail.com>
2023-03-14 00:02:13 +0000

    criaçao de nova compra

--- Controllers/NovaCompraController.cs
+++ Controllers/NovaCompraController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ShopShop.Data;
+using ShopShop.Migrations;
+using ShopShop.Models;
 
 namespace ShopShop.Controllers
 {
@@ -13,10 +15,46 @@ namespace ShopShop.Controllers
         {
             dbpointer = context;
         }
-        public IActionResult Index(string categoria)
+        public IActionResult Index(string categoria, string preco)
         {
             ViewBag.CATEGORIAS = new SelectList(dbpointer.Categorias, "Id", "Nome");
-            return View();
+
+            List<CategoriaProdutos> listaProdutos = new List<CategoriaProdutos>();
+
+            
+
+            if (categoria != null)
+            {
+                foreach (var item in dbpointer.Produtos)
+                {
+                    CategoriaProdutos produtoFiltrado = new CategoriaProdutos();
+                    if (Convert.ToInt32(categoria) == item.CategoriaId)
+                    {
+                        produtoFiltrado.Id = item.Id;
+                        produtoFiltrado.Nome = item.Nome;
+                        produtoFiltrado.PrecoCont = item.PrecoCont;
+                        produtoFiltrado.PrecoPD = item.PrecoPD;
+                        produtoFiltrado.PrecoMerc = item.PrecoMerc;
+                        listaProdutos.Add(produtoFiltrado);
+                    }                
+                }
+                if (preco != null)
+                {
+                    string[] precoParts = preco.Split('|');
+                    string id = precoParts[0];
+                    string local = precoParts[1];
+                    ViewBag.ID = id;
+                    ViewBag.LOCAL = local;
+                }
+                else
+                {
+                    ViewBag.NOME = "";
+                    ViewBag.LOCAL = "";
+                }
+                return View(listaProdutos.ToList());
+            }           
+
+            return View(listaProdutos.ToList());
         }
     }
 }
---
+++ Models/CategoriaProdutos.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopShop.Models
+{
+    public class CategoriaProdutos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double? PrecoCont { get; set; }
+        public double? PrecoPD { get; set; }
+        public double? PrecoMerc { get; set; }
+    }
+}
